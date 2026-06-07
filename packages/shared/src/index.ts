23e32a5370906eb1a0ca7fd841045f14c42f1074
Dev Vashist <devvashist1117@gmail.com>
2026-06-08 00:48:58 +0530

    fix: explicit index paths in shared exports

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,5 @@
-export * from './schemas';
-export * from './types';
+export * from './schemas/index';
+export * from './types/index';
 export * from './constants/quiz-questions';
 export * from './constants/this-or-that';
 export * from './constants/comparisons';
